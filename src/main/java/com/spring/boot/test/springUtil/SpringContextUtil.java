6e93a2eb8acb90bc67e429827bbb4157fe39342a
Administrator <842404548@qq.com>
2018-07-02 16:19:39 +0800

    spring工具类

--- src/main/java/com/spring/boot/test/springUtil/SpringContextUtil.java
+++ src/main/java/com/spring/boot/test/springUtil/SpringContextUtil.java
@@ -28,6 +28,7 @@ public class SpringContextUtil implements ApplicationContextAware {
      *
      * @param applicationContext
      */
+
     public void setApplicationContext(ApplicationContext applicationContext) {
         SpringContextUtil.applicationContext = applicationContext;
     }
